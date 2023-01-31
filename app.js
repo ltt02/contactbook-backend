87e67a83f63ed88fb9f86c10764a1332c8f44f62
ltt02 <lytantai02@gmail.com>
2023-01-31 16:16:55 +0700

    Dinh nghia cac routes cho tai nguyen Contact

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const cors = require('cors');
 
+const contactsRouter = require('./app/routes/contatct.routes');
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use("/api/contact", contactsRouter);
 
 app.get('/', (req, res) => {
     res.json({ message: "Welcome to contact book application."});

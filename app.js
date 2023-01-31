5ed0357f642533551ea70597dfcd1ae09cec45b6
ltt02 <lytantai02@gmail.com>
2023-01-31 16:28:08 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 
 const contactsRouter = require('./app/routes/contatct.routes');
+const ApiError = require('./app/api-error');
 
 const app = express();
 
@@ -10,8 +11,18 @@ app.use(express.json());
 
 app.use("/api/contact", contactsRouter);
 
+app.use((req, res, next) => {
+    return next(new ApiError(404, "Resource not found"));
+});
+
+app.use((err, req, res, next) => {
+    return res.status(err.statusCode || 500).json({
+        message: err.message || "Internal Server Error"
+    });
+})
+
 app.get('/', (req, res) => {
     res.json({ message: "Welcome to contact book application."});
-})
+});
 
 module.exports = app;
